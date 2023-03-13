adff94b7cff97e14a0372f05b2a49ace98f6aba5
Rajdeep <rajdeep.hascode@gmail.com>
2023-03-13 13:56:47 +0530

    header

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const hamburgerButton = document.querySelector('.hamburger-button');
+const navMenu = document.querySelector('.nav-menu');
+const header = document.querySelector('header');
+
+hamburgerButton.addEventListener('click', () => {
+    hamburgerButton.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    header.classList.toggle('active');
+
+});
